ff23eece407a9ba71cae32bd5e8ef7bd4a9e4219
MinhThu <18128062@student.hcmute.edu.vn>
2022-05-04 08:53:03 +0700

    updated code

--- flutter/lib/generated_plugin_registrant.dart
+++ flutter/lib/generated_plugin_registrant.dart
@@ -7,7 +7,9 @@
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:location_web/location_web.dart';
+import 'package:toast/toast_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
@@ -16,7 +18,9 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
+  ToastWebPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
