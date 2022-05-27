b47c7f3647ca4b299d768613edf7e78b076b1a8b
Tran Van An <51824455+tranan2509@users.noreply.github.com>
2022-05-28 02:13:29 +0700

    commit code notification

--- flutter/lib/generated_plugin_registrant.dart
+++ flutter/lib/generated_plugin_registrant.dart
@@ -7,6 +7,8 @@
 
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:location_web/location_web.dart';
@@ -20,6 +22,8 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   FirebaseFirestoreWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
