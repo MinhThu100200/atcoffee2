4c11797e727c9b1182b87c5b933ebcd954b702c2
MinhThu <18128062@student.hcmute.edu.vn>
2022-06-01 05:24:10 +0700

    commit pull

--- flutter/lib/constant/api_constants.dart
+++ flutter/lib/constant/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static String HOST = 'https://ed22-113-161-57-17.ap.ngrok.io';
+  static String HOST = 'https://eb1c-116-108-122-110.ap.ngrok.io';
   static String GET_CATEGORY = '/api/info/category/?list=true';
   static String GET_STORE = '/api/info/store';
   static String GET_ADRRESS(depth) =>
